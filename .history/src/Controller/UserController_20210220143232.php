d9569d6664c1c39e5d4352ed2d1c3bcd45ad9bdf
Bartosz Pazdur <b.pazdur@onet.pl>
2021-02-20 15:08:24 +0100

    Fixed date parsing

---
+++ .history/src/Controller/UserController_20210220143232.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Psr\Log\LoggerInterface;
+use App\Entity\User;
+
+class UserController extends AbstractController {
+    function index(LoggerInterface $logger) {
+        $users = $this->getDoctrine()
+                        ->getRepository(User::class)
+                        ->findAll();
+
+        $logger->info("Finding users");
+
+        if(count($users) === 0) {
+            $logger->warning("No users");
+            return new Response('Could not find any users', 404);
+        }
+
+        return $this->render('users/index.html.twig', [
+            "users" => $users
+        ]);
+    }
+
+    function create() {
+        return $this->render('users/create.html.twig');
+    }
+
+    function store(Request $request, LoggerInterface $logger) {
+        $new_user = new User();
+        $new_user->name = $request->request->get('name');
+        $new_user->email = $request->request->get('email');
+        $new_user->password = $request->request->get('email');
+
+        print_r()
+    }
+}
